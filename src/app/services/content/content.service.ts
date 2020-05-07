7d181d7def5a5c2c3b50ff68f1beac0bf82b1a73
Dev <n.eccles14@gmail.com>
2020-05-07 22:03:48 +0100

    Changed data return function. Added in check for set and type.

--- src/app/services/content/content.service.ts
+++ src/app/services/content/content.service.ts
@@ -1,22 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Observable, BehaviorSubject } from 'rxjs';
+import { content } from 'src/assets/content';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContentService {
-
   public content: Observable<{}>;
+  private _content = content;
 
-  constructor() { 
-    this.content =  new BehaviorSubject({});
-   }
+  constructor() {
+    this.content = new BehaviorSubject({});
+  }
 
-   getContent(setName: string, typeId: number): Observable<{}> {
-     console.log(setName);
-     console.log(typeId);
-     
-     
-     return this.content;
-   }
+  getContent(setName: string, typeId?: number) {
+    if (this._content[setName]) {
+      if (this._content[setName][typeId]) {
+        return this._content[setName][typeId];
+      } else {
+        return { error: 'Content not found' };
+      }
+    } else {
+      return { error: 'Content not found' };
+    }
+  }
 }
