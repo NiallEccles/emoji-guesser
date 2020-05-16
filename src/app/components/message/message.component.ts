429e91225cd33d22923100dd9640b5b116d7ab51
Dev <n.eccles14@gmail.com>
2020-05-16 13:08:41 +0100

    Added in message service and component.

---
+++ src/app/components/message/message.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'src/app/services/message/message.service';
+
+@Component({
+  selector: 'app-message',
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.scss']
+})
+export class MessageComponent implements OnInit {
+
+  public emojiIcon = '0x1F973';
+  public title = 'Congratulations!';
+  public show: boolean;
+
+  constructor(private messageService: MessageService) { 
+    this.messageService.show.subscribe(data=>{
+      this.show = data;
+    })
+   }
+
+  ngOnInit(): void {
+  }
+
+  public close() {
+    this.messageService.show.next(false);
+  }
+
+}
