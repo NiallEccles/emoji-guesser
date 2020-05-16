18c8e8892e2be2e5e3160ccf95ddb6d4ce2ac609
Dev <n.eccles14@gmail.com>
2020-05-16 13:09:09 +0100

    Updated progress and bottombar to update message service.

--- src/app/components/progress/progress.component.ts
+++ src/app/components/progress/progress.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProgressService } from 'src/app/services/progress/progress.service';
+import { MessageService } from 'src/app/services/message/message.service';
 
 @Component({
   selector: 'app-progress',
@@ -10,9 +11,12 @@ export class ProgressComponent implements OnInit {
   public numCompleted: number;
   public numRequired: number;
 
-  constructor(private progressService: ProgressService) {
+  constructor(private progressService: ProgressService, private messageService: MessageService) {
     this.progressService.numCompleted.subscribe((data) => {
       this.numCompleted = data;
+      if(this.numCompleted === this.numRequired){
+        this.messageService.show.next(true);
+      }
     });
 
     this.progressService.numRequired.subscribe((data) => {
