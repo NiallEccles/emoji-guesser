a409018f21c12c50043513ce081f13180572c14a
Dev <n.eccles14@gmail.com>
2020-05-17 22:04:11 +0100

    Added emoji to London object.

--- src/assets/content.ts
+++ src/assets/content.ts
@@ -26,10 +26,78 @@ export const content = {
         },
       ],
     },
+    2: {
+      name: 'Dublin',
+      description: 'Overall',
+      theme: 'blue',
+      difficulty: 'normal',
+      emoji: [0x1F3DD],
+      content: [
+        {
+          name: 'Bull Island',
+          hints: [[0x1f402], [0x1f3dd, 0xfe0f]],
+          acc: ['bill island'],
+        },
+        {
+          name: 'Lucan',
+          hints: [[0x1f6bd], [0x1f96b]],
+        },
+        {
+          name: 'Islandbridge',
+          hints: [[0x1f3dd, 0xfe0f],[0x1F309]],
+        },
+        {
+          name: 'Dolphins Barn',
+          hints: [[0x1F42C],[0x1F3E0],[0x1F404],[0x1F40E], [0x1F416], [0x1F411], [0x1F413]],
+        },
+      ],
+    },
+    3: {
+      name: 'Dublin',
+      description: 'Overall',
+      theme: 'blue',
+      difficulty: 'normal',
+      emoji: [0x1F3DD],
+      content: [
+        {
+          name: 'Bull Island',
+          hints: [[0x1f402], [0x1f3dd, 0xfe0f]],
+          acc: ['bill island'],
+        },
+        {
+          name: 'Lucan',
+          hints: [[0x1f6bd], [0x1f96b]],
+        },
+        {
+          name: 'Islandbridge',
+          hints: [[0x1f3dd, 0xfe0f],[0x1F309]],
+        },
+        {
+          name: 'Dolphins Barn',
+          hints: [[0x1F42C],[0x1F3E0],[0x1F404],[0x1F40E], [0x1F416], [0x1F411], [0x1F413]],
+        },
+      ],
+    }
   },
   London: {
     1: {
       name: 'London',
+      description: 'Overall',
+      theme: 'green',
+      difficulty: 'normal',
+      emoji: [0x1F3DD],
+      content: [
+        {
+          name: 'Waterloo',
+          hints: [[0x1f4a6], [0x1f6bd]],
+          acc: ['water loo'],
+        },
+      ],
+    },
+  },
+  Madrid: {
+    1: {
+      name: 'Madrid',
       description: 'Overall',
       theme: 'green',
       difficulty: 'normal',
