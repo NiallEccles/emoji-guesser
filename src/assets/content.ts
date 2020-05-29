62ca726d65546756a986e85651e6b631a25835da
Dev <n.eccles14@gmail.com>
2020-05-29 21:34:46 +0100

    Took out Madrid. Added in some more emojies for Dublin.

--- src/assets/content.ts
+++ src/assets/content.ts
@@ -1,8 +1,8 @@
 export const content = {
   Dublin: {
     1: {
-      name: 'Dublin',
-      description: 'Overall',
+      name: 'Dublin Places',
+      description: 'Can you name all these places in Dublin?',
       theme: 'blue',
       difficulty: 'normal',
       emoji: [0x1F3DD],
@@ -23,12 +23,36 @@ export const content = {
         {
           name: 'Dolphins Barn',
           hints: [[0x1F42C],[0x1F3E0],[0x1F404],[0x1F40E], [0x1F416], [0x1F411], [0x1F413]],
+        },
+        {
+          name: 'Swords',
+          hints: [[0x2694],[0x2694],[0x2694]],
+        },
+        {
+          name: 'Bluebell',
+          hints: [[0x1F630],[0x1F514]],
+        },
+        {
+          name: 'Castleknock',
+          hints: [[0x1F3F0],[0x270A],[0x1F6AA]],
+        },
+        {
+          name: 'Foxrock',
+          hints: [[0x1F98A],[0x1F3B8],[0x1F941],[0x1F3B8]],
+        },
+        {
+          name: 'Cabra',
+          hints: [[0x1F695],[0x1F459]],
+        },
+        {
+          name: 'Coolock',
+          hints: [[0x2744],[0x1F512]],
         },
       ],
     },
     2: {
-      name: 'Dublin',
-      description: 'Overall',
+      name: 'Dublin Pubs',
+      description: 'Name these watering holes.',
       theme: 'blue',
       difficulty: 'normal',
       emoji: [0x1F3DD],
@@ -52,32 +76,6 @@ export const content = {
         },
       ],
     },
-    3: {
-      name: 'Dublin',
-      description: 'Overall',
-      theme: 'blue',
-      difficulty: 'normal',
-      emoji: [0x1F3DD],
-      content: [
-        {
-          name: 'Bull Island',
-          hints: [[0x1f402], [0x1f3dd, 0xfe0f]],
-          acc: ['bill island'],
-        },
-        {
-          name: 'Lucan',
-          hints: [[0x1f6bd], [0x1f96b]],
-        },
-        {
-          name: 'Islandbridge',
-          hints: [[0x1f3dd, 0xfe0f],[0x1F309]],
-        },
-        {
-          name: 'Dolphins Barn',
-          hints: [[0x1F42C],[0x1F3E0],[0x1F404],[0x1F40E], [0x1F416], [0x1F411], [0x1F413]],
-        },
-      ],
-    }
   },
   London: {
     1: {
@@ -86,21 +84,6 @@ export const content = {
       theme: 'green',
       difficulty: 'normal',
       emoji: [0x1F3DD],
-      content: [
-        {
-          name: 'Waterloo',
-          hints: [[0x1f4a6], [0x1f6bd]],
-          acc: ['water loo'],
-        },
-      ],
-    },
-  },
-  Madrid: {
-    1: {
-      name: 'Madrid',
-      description: 'Overall',
-      theme: 'green',
-      difficulty: 'normal',
       content: [
         {
           name: 'Waterloo',
